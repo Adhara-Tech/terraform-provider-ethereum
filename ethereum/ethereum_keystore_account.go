b48d8125dccf8378b79d4010f44a9347cc978288
figuerascarlos <carlosd.figueras@gmail.com>
2020-07-13 18:27:49 +0200

    Implement update operation

--- ethereum/ethereum_keystore_account.go
+++ ethereum/ethereum_keystore_account.go
@@ -17,6 +17,7 @@ func resourceEthereumKeystore() *schema.Resource {
 
 	return &schema.Resource{
 		Create: CreateAccount,
+		Update: UpdateAccount,
 		Delete: DeleteAccount,
 		Read:   ReadAccount,
 
@@ -25,20 +26,17 @@ func resourceEthereumKeystore() *schema.Resource {
 				Type:        schema.TypeString,
 				Optional:    true,
 				Description: "scrypt encryption difficulty ( lightkdf => 4MB and 100ms CPU approximately, standartkdf => 256MB and 1s CPU approximately",
-				ForceNew:    true,
 				Default:     "lightkdf",
 			},
 			"passphrase": {
 				Type:        schema.TypeString,
 				Optional:    true,
 				Description: "passphrase to encrypt the ethereum account",
-				ForceNew:    true,
 				Default:     "",
 			},
 			"public_address": {
-				Type:      schema.TypeString,
-				Computed:  true,
-				Sensitive: true,
+				Type:     schema.TypeString,
+				Computed: true,
 			},
 			"private_key": {
 				Type:      schema.TypeString,
@@ -54,34 +52,36 @@ func resourceEthereumKeystore() *schema.Resource {
 	}
 }
 
+func getScryptValues(scryptEncryption string) (scriptN, scriptP int, err error) {
+
+	switch scryptEncryption {
+
+	case "lightkdf":
+		scriptN = 4096
+		scriptP = 6
+		err = nil
+		return
+	case "standartkdf":
+		scriptN = 262144
+		scriptP = 1
+		err = nil
+		return
+	default:
+		return 0, 0, fmt.Errorf("invalid script encryption. Should be lightkdf or standartkdf")
+	}
+}
+
 func CreateAccount(d *schema.ResourceData, meta interface{}) error {
 
 	scryptEncryption := d.Get("scrypt_encryption").(string)
 	passphrase := d.Get("passphrase").(string)
-
-	getScryptValues := func() (scriptN, scriptP int, err error) {
-		switch scryptEncryption {
-		case "lightkdf":
-			scriptN = 4096
-			scriptP = 6
-			err = nil
-			return
-		case "standartkdf":
-			scriptN = 262144
-			scriptP = 1
-			err = nil
-			return
-		default:
-			return 0, 0, fmt.Errorf("invalid script encryption. Should be lightkdf or standartkdf")
-		}
-	}
 
 	k, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 	if err != nil {
 		return fmt.Errorf("error generating ecdsa key: %s", err)
 	}
 
-	scriptN, scriptP, err := getScryptValues()
+	scriptN, scriptP, err := getScryptValues(scryptEncryption)
 	if err != nil {
 		return err
 	}
@@ -107,9 +107,46 @@ func CreateAccount(d *schema.ResourceData, meta interface{}) error {
 		return fmt.Errorf("error parsing EncryptedKeyV3 json")
 	}
 
-	d.SetId(hashForState(publicAddress))
+	d.SetId(publicAddress)
 	d.Set("private_key", privateKey)
 	d.Set("public_address", publicAddress)
+	d.Set("encrypted_json", string(encryptedJSON))
+
+	return nil
+}
+
+func UpdateAccount(d *schema.ResourceData, meta interface{}) error {
+
+	scryptEncryption := d.Get("scrypt_encryption").(string)
+	passphrase := d.Get("passphrase").(string)
+
+	scriptN, scriptP, err := getScryptValues(scryptEncryption)
+	if err != nil {
+		return err
+	}
+	privateKeyBytes, err := hex.DecodeString(d.Get("private_key").(string))
+	if err != nil {
+		return fmt.Errorf("error decoding private key")
+	}
+
+	var keystoreJSONV3 utils.EncryptedKeyV3
+	err = json.Unmarshal([]byte(d.Get("encrypted_json").(string)), &keystoreJSONV3)
+	if err != nil {
+		return fmt.Errorf("error unmarshaling the encryptedKeyV3")
+	}
+
+	crytoJSON, err := utils.EncryptJSONV3(privateKeyBytes, []byte(passphrase), scriptN, scriptP)
+	if err != nil {
+		return err
+	}
+
+	keystoreJSONV3.Crypto = crytoJSON
+
+	encryptedJSON, err := json.Marshal(keystoreJSONV3)
+	if err != nil {
+		return fmt.Errorf("error parsing EncryptedKeyV3 json")
+	}
+
 	d.Set("encrypted_json", string(encryptedJSON))
 
 	return nil
--- ethereum/provider.go
+++ ethereum/provider.go
@@ -1,21 +1,9 @@
 package ethereum
 
 import (
-	"crypto/sha1"
-	"encoding/hex"
-	"strings"
-
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
-
-func hashForState(value string) string {
-	if value == "" {
-		return ""
-	}
-	hash := sha1.Sum([]byte(strings.TrimSpace(value)))
-	return hex.EncodeToString(hash[:])
-}
 
 func Provider() terraform.ResourceProvider {
 	return &schema.Provider{
